593bab15e0510b237fae64bd7a86b2306f2c7690
Javier Arias <parias@heinsohn.com>
2026-06-10 13:27:53 -0500

    Se ajusta adaptador UserValidationAdapter para integrar token en la petición

--- src/main/java/com/pragma/powerup/infrastructure/out/http/adapter/UserValidationAdapter.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/http/adapter/UserValidationAdapter.java
@@ -4,9 +4,15 @@ import com.pragma.powerup.domain.exception.TechnicalException;
 import com.pragma.powerup.domain.exception.TechnicalExceptionResponse;
 import com.pragma.powerup.domain.spi.IUserValidationPort;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.lang.NonNull;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @RequiredArgsConstructor
 public class UserValidationAdapter implements IUserValidationPort {
@@ -18,12 +24,26 @@ public class UserValidationAdapter implements IUserValidationPort {
     public boolean isOwner(Long userId) {
         try {
             String url = usersServiceUrl + "/api/v1/user/" + userId + "/is-owner";
-            Boolean result = restTemplate.getForObject(url, Boolean.class);
+            HttpEntity<Void> request = new HttpEntity<>(buildAuthHeaders());
+            Boolean result = restTemplate.exchange(url, HttpMethod.GET, request, Boolean.class).getBody();
             return Boolean.TRUE.equals(result);
         } catch (HttpClientErrorException.NotFound e) {
             return false;
         } catch (ResourceAccessException e) {
             throw new TechnicalException(TechnicalExceptionResponse.USER_VALIDATION_UNAVAILABLE.getMessage());
         }
+    }
+
+    @NonNull
+    private HttpHeaders buildAuthHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes != null) {
+            String authHeader = attributes.getRequest().getHeader(HttpHeaders.AUTHORIZATION);
+            if (authHeader != null) {
+                headers.set(HttpHeaders.AUTHORIZATION, authHeader);
+            }
+        }
+        return headers;
     }
 }
